ea1c852e3c6eea8f1401491d698a89e5fb275139
Joan Vendrell <71008133+joanvendrell@users.noreply.github.com>
2025-05-11 20:54:36 -0700

    update main folder

--- main/FaultDetection.py
+++ main/FaultDetection.py
@@ -1,24 +1,58 @@
 ## ------- Fault-Detection Mechanism ----- ##
 import numpy as np
-from Auxiliary import get_combinations
+import sys
+import os
 import random
 
-# Fault-detection mechaism
-def fault_detection(selected,trace_curvature,curvatures,strategy):
+sys.path.append(os.path.abspath(os.path.join(os.getcwd(), '../')))
+from auxiliary.Auxiliary import get_combinations, compute_coverage
+
+# Fault-detection mechanism
+def fault_detection_coverage(input_data,S,e,path_curvature,set_curvature,tau,strategy):
     if strategy == "ResQue":
-        z = np.random.rand(1)
-        gamma = max(max(trace_curvature,default=0),max(curvatures,default=0))
-        if z <= gamma/(1+gamma):
+        curvature = path_curvature[-1] if path_curvature else 0
+        if curvature < max(set_curvature,default=0):
+            return True
+    elif strategy == "Modularity":
+        f_S = len(compute_coverage(input_data,S)); f_e = len(compute_coverage(input_data,[e]))
+        f_S_e = len(compute_coverage(input_data,S+[e]))
+        if f_S_e <= (f_e + f_S)*tau:
+            return True
+    elif strategy == "Probabilistic":
+        gamma = max(max(path_curvature,default=0),max(set_curvature,default=0))
+        if np.random.rand(1) <= gamma/(1+gamma):
             return True
     elif strategy == "Random":
-        z1 = np.random.rand(1); z2 = np.random.rand(1)
-        if z1 >= z2:
+        if np.random.rand(1) >= 0.5:
+            return True
+    return False
+
+def fault_detection_reachability(G,reachability_dict,S,e,path_curvature,set_curvature,tau,strategy):
+    if strategy == "ResQue":
+        curvature = path_curvature[-1] if path_curvature else 0
+        if curvature <= max(set_curvature,default=0):
+            return True
+    elif strategy == "Modularity":
+        f_S = prepare_probabilistic_reachability_sets(G, reachability_dict, S, seed)
+        f_e = prepare_probabilistic_reachability_sets(G, reachability_dict, [e], seed)
+        f_S_e = prepare_probabilistic_reachability_sets(G, reachability_dict, S+[e], seed)
+        if f_S_e <= (f_e + f_S)*tau:
+            return True
+    elif strategy == "Probabilistic":
+        gamma = max(max(path_curvature,default=0),max(set_curvature,default=0))
+        if np.random.rand(1) <= gamma/(1+gamma):
+            return True
+    elif strategy == "Random":
+        if np.random.rand(1) >= 0.5:
             return True
     return False
 
 # Re-wiring strategy
 def rewiring(groundset,selected,trace_curvature,strategy):
-    if strategy == "ResQue":
+    if strategy == "Random":
+        options = get_combinations(groundset, len(selected))
+        return list(random.choice(options)), trace_curvature
+    else:
         differences = [abs(trace_curvature[i] - trace_curvature[i-1]) for i in range(1, len(trace_curvature))]
         if not differences:
             return selected, trace_curvature
@@ -26,6 +60,3 @@ def rewiring(groundset,selected,trace_curvature,strategy):
         selected.remove(element)
         trace_curvature.remove(trace_curvature[differences.index(max(differences))])
         return selected, trace_curvature
-    elif strategy == "Random":
-        options = get_combinations(groundset, len(selected))
-        return list(random.choice(options)), trace_curvature
--- main/ResQueGreedy.py
+++ main/ResQueGreedy.py
@@ -1,28 +1,106 @@
 ## ----- ResQue Greedy Algorithm ----- ###
 import numpy as np
-from FaultDetection import fault_detection, rewiring
-from Auxiliary import compute_marginals
+import sys
+import os
+from tqdm import tqdm
+from .FaultDetection import fault_detection_coverage,fault_detection_reachability,rewiring
+sys.path.append(os.path.abspath(os.path.join(os.getcwd(), '../')))
+from auxiliary.Auxiliary import *
 
 # Auxiliary
-def greedy_step(marginals):
-    return np.argmax(marginals)
+def greedy_step(marginals, input_data):
+    idx = np.argmax(marginals)
+    return list(input_data.keys())[idx]
 
 
 # Main Algorithm
-def greedy_coverage(allocation_points, target_points, radius, k, strategy = "Classical"):
+# input_data = dict{allocation_points: target_points}
+def greedy_coverage(ground_set, input_data, k, strategy = "Classical", rewiring_limit = 100000, tau = 0.8, monitor=True):
     selected = []
-    trace_curvature = []; curvatures = []  
-    query_time = 0; rewires = 0
+    path_curvature = []; set_curvature = []  
+    query_time = 0; rewires = 0; iters = 0
+    r_iter=0; controlled_iters=0; last_seq_greedy_curvatures=None; accumulated_cond=0
+    last_greedy_S = None
     while len(selected)<k:
-        # Re-wired
-        if strategy != "Classical" and rewires<2:
-            if fault_detection(selected,trace_curvature,curvatures,strategy):
-                selected,trace_curvature = rewiring(list(range(len(allocation_points))),selected,trace_curvature,strategy)
-                rewires += 1
+        iters += 1
         # Apply Greedy Algorithm
-        marginals,curvatures,queries = compute_marginals(allocation_points, target_points, radius, selected)
-        e = greedy_step(marginals)
+        S_i_minus_1 = selected
+        marginals, curvature_expansion, queries = compute_marginals(input_data, selected)
+        e = greedy_step(marginals, input_data)
         selected.append(e)
-        trace_curvature.append(curvatures[e])
+        path_curvature.append(curvature_expansion[e])
+        set_curvature.append(max(curvature_expansion))
         query_time += queries
-    return selected,query_time,rewires,trace_curvature
+        # Monitoring sufficient conditions
+        if monitor and r_iter == 1:
+            delta_rsg = len(compute_coverage(input_data,S_i_minus_1+[e])) - len(compute_coverage(input_data,S_i_minus_1))
+            delta_sg = len(compute_coverage(input_data,last_greedy_S)) - len(compute_coverage(input_data,S_i_minus_1))
+            accumulated_cond += delta_rsg - delta_sg
+        # Re-wired        
+        if strategy == "Classical":
+            pass
+        elif strategy != "Classical" and rewires<rewiring_limit:
+            if fault_detection_coverage(input_data,S_i_minus_1,e,path_curvature,set_curvature,tau,strategy):
+                # Re-wiring procedure
+                selected,path_curvature = rewiring(list(input_data.keys()),selected,path_curvature,strategy)
+                rewires += 1
+            # Monitoring sufficient conditions
+            if monitor:
+                if rewires == 1 and r_iter == 0:
+                    last_seq_greedy_curvatures = curvature_expansion; r_iter += 1
+                    last_greedy_S = S_i_minus_1
+                elif rewires >= 1 and r_iter > 0:
+                    r_iter += 1; lhs = (curvature_expansion[e] - last_seq_greedy_curvatures[e])*len(compute_coverage(input_data,[e]))
+                    if accumulated_cond >= lhs:
+                        controlled_iters += 1
+                        accumulated_cond += lhs
+    if monitor:
+        uncertainty = 1-controlled_iters/max(1,r_iter-1)
+    else:
+        uncertainty = 1
+    return selected,query_time,rewires,max(set_curvature),uncertainty
+
+def greedy_reachability(G,reachability_dict,k,strategy="Classical",rewiring_limit=100000,seed=None,num_workers=3,tau=0.5,monitor=True):
+    selected = []
+    path_curvature = []; set_curvature = []  
+    query_time = 0; rewires = 0; iters = 0
+    r_iter=0; controlled_iters=0; last_seq_greedy_curvatures=None; accumulated_cond=0
+    last_greedy_S = None
+    while len(selected)<k:
+        iters += 1
+        # Apply Greedy Algorithm
+        S_i_minus_1 = selected
+        marginals, curvature_expansion, queries = compute_marginals_reachability(G,reachability_dict,selected,seed)
+        e = np.argmax(marginals) #greedy_step(marginals, input_data)
+        selected.append(e)
+        path_curvature.append(curvature_expansion[e])
+        set_curvature.append(max(curvature_expansion))
+        query_time += queries
+        # Monitoring sufficient conditions
+        if monitor and r_iter == 1:
+            delta_rsg = prepare_probabilistic_reachability_sets(G, reachability_dict, S_i_minus_1+[e], seed) - prepare_probabilistic_reachability_sets(G, reachability_dict, S_i_minus_1, seed)
+            delta_sg = prepare_probabilistic_reachability_sets(G, reachability_dict, last_greedy_S, seed) - prepare_probabilistic_reachability_sets(G, reachability_dict, S_i_minus_1, seed)
+            accumulated_cond += delta_rsg - delta_sg
+        # Re-wired        
+        if strategy == "Classical":
+            pass
+        elif strategy != "Classical" and rewires<rewiring_limit:
+            if fault_detection_reachability(G,reachability_dict,S_i_minus_1,e,path_curvature,set_curvature,tau,strategy):
+                # Re-wiring procedure
+                selected,path_curvature = rewiring(list(G.nodes()),selected,path_curvature,strategy)
+                rewires += 1
+            # Monitoring sufficient conditions
+            if monitor:
+                if rewires == 1 and r_iter == 0:
+                    last_seq_greedy_curvatures = curvature_expansion; r_iter += 1
+                    last_greedy_S = S_i_minus_1
+                elif rewires >= 1 and r_iter > 0:
+                    r_iter += 1; lhs = (curvature_expansion[e] - last_seq_greedy_curvatures[e])*prepare_probabilistic_reachability_sets(G, reachability_dict, [e], seed)
+                    if accumulated_cond >= lhs:
+                        controlled_iters += 1
+                        accumulated_cond += lhs
+    if monitor:
+        uncertainty = 1-controlled_iters/max(1,r_iter-1)
+    else:
+        uncertainty = 1
+    return selected,query_time,rewires,max(set_curvature),uncertainty
